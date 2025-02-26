ea24d2f99df27aa234ad85d01a8b33a427740cba
env-gaurav <gaurav2528singh@gmail.com>
2025-02-26 23:57:47 +0530

    implemented multiplication using for and while loop

---
+++ table_using_for_loop.py
@@ -0,0 +1,8 @@
+def multipleOf(n):
+    for i  in range(1,11):
+        print( n,"x",i,"=",i*n)
+    print()
+
+multipleOf(10)
+multipleOf(12)
+multipleOf(-2)
---
+++ table_using_while_loop.py
@@ -0,0 +1,9 @@
+def multipleOf(n):
+    start=1
+    end=11
+    while start<end:
+        print(n,"x",start,"=",n*start)
+        start+=1
+    print()
+
+multipleOf(5)
